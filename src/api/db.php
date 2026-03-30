8281d373dcd71c617d21a2bbd2c1637a9f26824c
Alex <aloney.cpp@gmail.com>
2026-03-29 21:05:08 -0700

    Updating compose and db access config

--- src/api/db.php
+++ src/api/db.php
@@ -7,6 +7,7 @@ try {
     $pdo = new PDO($dsn, $username, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
     ]);
 } catch (PDOException $e) {
     header('Content-Type: application/json');
